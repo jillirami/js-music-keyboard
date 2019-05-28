07d27b5038427f063eef733e0cefb83731deaa0f
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-28 16:58:20 -0700

    solution complete

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    $(`#${$(this).attr('class').split(' ')[1]}Audio`)[0].load();
+    $(`#${$(this).attr('class').split(' ')[1]}Audio`)[0].play();
+  });
+
+  const availableKeys = {"c":1, "d":1, "e":1, "f":1, "g":1, "a":1, "b":1};
+
+  $('body').keydown((event) => {
+    if (availableKeys[event.key] !== undefined) {
+      $(`#${event.key}Audio`)[0].load();
+      $(`#${event.key}Audio`)[0].play();
+    }
+  });
 });
